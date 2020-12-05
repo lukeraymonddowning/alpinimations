3a93573ff3f82da813cc2474b1135e26ac583db7
luke <lukeraymonddowning@gmail.com>
2020-12-05 08:16:35 +0000

    Updates an changed animation and adds a new animation.

--- resources/views/tailwindui/dropdown/panel.blade.php
+++ resources/views/tailwindui/dropdown/panel.blade.php
@@ -1,4 +1,4 @@
-x-transition:enter="transition ease-out duration-200"
+x-transition:enter="transition ease-out duration-100"
 x-transition:enter-start="transform opacity-0 scale-95"
 x-transition:enter-end="transform opacity-100 scale-100"
 x-transition:leave="transition ease-in duration-75"
